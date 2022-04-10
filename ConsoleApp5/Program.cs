910e43a03a2f838e078e9fb71cbf611ac4c81d50
PolyanskayaAR <asya150203@gmail.com>
2022-04-10 11:59:47 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+        }
+        enum Semaphore
+        {
+            Red = 100,
+            Yellow = 200,
+            Green = 300
+        }
+    }
+}
